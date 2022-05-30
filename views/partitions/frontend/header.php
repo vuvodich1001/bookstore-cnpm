98f169a9c77f466fdfb57e98b29f4d4c4d8e748f
Vuvodich1001 <vunguyen.311001@gmail.com>
2022-05-30 21:38:09 +0700

    first commit

--- views/partitions/frontend/header.php
+++ views/partitions/frontend/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookStore</title>
     <!-- base url-->
-    <base href="http://localhost/mvc-php/">
+    <base href="http://localhost/bookstore-cnpm/">
     <!-- font roboto -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
